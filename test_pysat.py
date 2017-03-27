d67b988b9aa780fba8ffb898da54923e949a3fa7
Lili Feng <lili.feng@colorado.edu>
2017-03-26 20:58:14 -0600

    Mar 26th, 2017

--- test_pysat.py
+++ test_pysat.py
@@ -3,25 +3,12 @@ import numpy as np
 # 
 eTensor=pysat.elasticTensor()
 eTensor.elastic_DB(mtype='ice')
-
-R=pysat.euler2mat(1,2,3)
+# eTensor.set_love(A=15.55, C=11.96, L=4.76, N=5.33, F=3.99, mtype='HTI')
 # eTensor.Voigt2Cijkl()
-# eTensor.set_radial(8.7, 4.5, 3.3, 0.1, 0.5, 1)
-# eTensor.set_love(A=13.961, C=15.013, L=3.21, N=3.404, F=5.765)
-# eTensor.set_thomsen(vp=5.3, vs=3.2, eps=0.1, gamma=0.05, delta=0.00, rho=4000)
-# # eTensor.Voigt2Cijkl()
-# eTensor.rotB([0,1,0], 90.)
-# eTensor.rotB([1,0,0], 90.)
-# eTensor.rotTB([0,0,1], 90.)
-# eTensor.Cijkl2Voigt()
-# 
-# # 
-# eTensor2=pysat.elasticTensor()
-# # # eTensor.Voigt2Cijkl()
-# # 
-# eTensor2.set_love(A=13.961, C=15.013, L=3.21, N=3.404, F=5.765)
-# # eTensor.Voigt2Cijkl()
-# eTensor2.rotT([0,1,0], 90.)
+eTensor.rot_dip_strike(dip=30., strike=50.)
 
-# eTensor2.rotB([1,0,0], 90.)
-# eTensor2.rotB([0,0,1], 90.)
+eTensor2=pysat.elasticTensor()
+eTensor2.elastic_DB(mtype='ice')
+# eTensor.set_love(A=15.55, C=11.96, L=4.76, N=5.33, F=3.99, mtype='HTI')
+# eTensor.Voigt2Cijkl()
+eTensor2.rot_dip_strike(dip=30., strike=50., method='euler')
